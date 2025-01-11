677693a80bad8d48ec0eb45c10a946d1c6524113
Revital Frid <revitalf512@gmail.com>
2025-01-11 02:42:18 +0200

    fix: remove not used import

--- app/src/main/java/com/example/safesound/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/safesound/ui/main/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.safesound.ui.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
